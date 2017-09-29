64fede064298e55a5ee0e1191ec5f20248fd6c02
undefined3394 <32277378+undefined3394@users.noreply.github.com>
2017-09-29 14:33:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,68 @@
 /**
-  Safe-Proxy v1.0 by undefined#3394
+  Safe-Proxy v1.1 by undefined#3394
   => Module that prevents proxy from sending crafted/modified packets to TERA Server.
   => TERA Client will still send packets that can get you banned if you change your location, use broker-anywhere, etc..
   => Will break most of modules but also prevent you from getting banned for sending malformed packets to server.
 */
 
+const crypto = require('crypto');
+const DELAY_TIMEOUT = 1000;
+
 module.exports = function SafeProxy(dispatch) {
-  const debug = true;
+  const debug = false;
   let enabled = true;
   
-  const packetHook = (type, code) => {
+  const silencedStack = {};
+  const silencedTimeoutStack = {};
+  
+  const removeHash = (code, hash) => {
+    const stack = silencedStack[code];
+    if(!stack) return;
+    const idx = stack.indexOf(hash);
+    if(idx === -1) return;
+    stack.splice(idx, 1);
+    if(!stack.length) delete silencedStack[code];
+    clearTimeout(silencedTimeoutStack[hash]);
+    delete silencedTimeoutStack[hash];
+  };
+  
+  const getPacketName = code => {
+    return dispatch.base.protocolMap.code.get(code) || code;
+  };
+  
+  const packetHook = (type, code, data) => {
     if(!enabled) return;
-    if(debug) {
-      const name = dispatch.base.protocolMap.code.get(code);
-      console.log(`[SafeProxy] blocking proxy-${type} outgoing packet`, (name || code));
+    
+    const stack = silencedStack[code];
+    if(stack && stack.length) {
+      const hash = crypto.createHash('md5').update(data).digest('hex');
+      if(stack.includes(hash)) {
+        //process.nextTick(() => {
+          removeHash(code, hash);
+        //});
+        if(debug) console.log(`[SafeProxy] allowed proxy-delayed packet`, getPacketName(code), silencedStack);
+        return;
+      }
     }
+    
+    if(debug) console.log(`[SafeProxy] blocked proxy-${type} outgoing packet`, getPacketName(code));
     return false;
   };
   
   dispatch.hook('*', 'raw', { filter: { fake: false, incoming: false, modified: true }, order: 10000 }, packetHook.bind(null, 'modified'));
   dispatch.hook('*', 'raw', { filter: { fake: true,  incoming: false }, order: 10000 }, packetHook.bind(null, 'crafted'));
+  
+  //---
+  
+  const silencedHook = (code, data) => {
+    process.nextTick(() => {
+      const hash = crypto.createHash('md5').update(data).digest('hex');
+      if(!(code in silencedStack)) silencedStack[code] = [ hash ];
+      silencedTimeoutStack[hash] = setTimeout(removeHash, DELAY_TIMEOUT, code, hash);
+    });
+  };
+  
+  dispatch.hook('*', 'raw', { filter: { fake: false, incoming: false, silenced: true }, order: 10000 }, silencedHook);
   
   //---
   
